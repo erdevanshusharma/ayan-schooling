9bb9d94d0ee68b8f3e062e149479f6f39b062404
Devanshu Sharma <devanshu@apple.com>
2024-09-23 11:31:47 -0700

    Made geography page generic as well, links are now part of the explaination markdown.

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,11 +1,43 @@
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
-import GeographyPage from "./GeographyPage";
 import { useLocation, useNavigate } from "react-router-dom";
 import SimpleQuestionAnswerView from "./SimpleQuestionAnswerView";
 
 type TabValue = "math" | "science" | "geography" | "englishGrammar";
+
+interface ISubjectConfig {
+  tabName: string;
+  dataUrl: string;
+  title: string;
+}
+
+const subjectConfig: ISubjectConfig[] = [
+  {
+    tabName: "math",
+    dataUrl:
+      "https://gist.githubusercontent.com/erdevanshusharma/104ae6bc9843f34512d0b1f559e7c582/raw/mathsQuestions.json",
+    title: "Maths Challenge",
+  },
+  {
+    tabName: "science",
+    dataUrl:
+      "https://gist.githubusercontent.com/erdevanshusharma/9e12748907fd91ce25a4d2fd23963e49/raw/scienceQuestions.json",
+    title: "Science Challenge",
+  },
+  {
+    tabName: "geography",
+    dataUrl:
+      "https://gist.githubusercontent.com/erdevanshusharma/84c09f63952963e9e7bb2d24c91b2e63/raw/geographyQuestions.json",
+    title: "Geography Challenge",
+  },
+  {
+    tabName: "english",
+    dataUrl:
+      "https://gist.githubusercontent.com/erdevanshusharma/32f73472dc5793a88a0c69eb449b791d/raw/englishGrammarQuestions.json",
+    title: "English Grammar Challenge",
+  },
+];
 
 const MainPage = () => {
   const navigate = useNavigate();
@@ -47,30 +79,14 @@ const MainPage = () => {
         className="w-full max-w-4xl mx-auto"
       >
         <TabsList className="grid w-full grid-cols-4 rounded-xl bg-white/20 p-1">
-          <TabsTrigger
-            value="math"
-            className="rounded-lg text-white hover:bg-white/10 data-[state=active]:bg-white/30"
-          >
-            Maths
-          </TabsTrigger>
-          <TabsTrigger
-            value="science"
-            className="rounded-lg text-white hover:bg-white/10 data-[state=active]:bg-white/30"
-          >
-            Science
-          </TabsTrigger>
-          <TabsTrigger
-            value="geography"
-            className="rounded-lg text-white hover:bg-white/10 data-[state=active]:bg-white/30"
-          >
-            Geography
-          </TabsTrigger>
-          <TabsTrigger
-            value="englishGrammar"
-            className="rounded-lg text-white hover:bg-white/10 data-[state=active]:bg-white/30"
-          >
-            English
-          </TabsTrigger>
+          {subjectConfig.map((config) => (
+            <TabsTrigger
+              value={config.tabName}
+              className="rounded-lg text-white hover:bg-white/10 data-[state=active]:bg-white/30"
+            >
+              <p className="capitalize">{config.tabName}</p>
+            </TabsTrigger>
+          ))}
         </TabsList>
         <motion.div
           key={activeTab}
@@ -79,33 +95,14 @@ const MainPage = () => {
           variants={tabVariants}
           transition={{ duration: 0.3 }}
         >
-          <TabsContent value="math">
-            <SimpleQuestionAnswerView
-              dataUrl={
-                "https://gist.githubusercontent.com/erdevanshusharma/104ae6bc9843f34512d0b1f559e7c582/raw/mathsQuestions.json"
-              }
-              title={"Maths Challenge"}
-            />
-          </TabsContent>
-          <TabsContent value="science">
-            <SimpleQuestionAnswerView
-              dataUrl={
-                "https://gist.githubusercontent.com/erdevanshusharma/9e12748907fd91ce25a4d2fd23963e49/raw/scienceQuestions.json"
-              }
-              title={"Science Challenge"}
-            />
-          </TabsContent>
-          <TabsContent value="geography">
-            <GeographyPage />
-          </TabsContent>
-          <TabsContent value="englishGrammar">
-            <SimpleQuestionAnswerView
-              dataUrl={
-                "https://gist.githubusercontent.com/erdevanshusharma/32f73472dc5793a88a0c69eb449b791d/raw/englishGrammarQuestions.json"
-              }
-              title={"English Grammar Challenge"}
-            />
-          </TabsContent>
+          {subjectConfig.map((config) => (
+            <TabsContent value={config.tabName} key={config.tabName}>
+              <SimpleQuestionAnswerView
+                dataUrl={config.dataUrl}
+                title={config.title}
+              />
+            </TabsContent>
+          ))}
         </motion.div>
       </Tabs>
     </div>
