badc6441081186755e34ef1088e99a2475e5ceb1
Devanshu Sharma <devanshu@apple.com>
2024-10-03 10:13:44 -0700

    Added ethics and converted the tab into a dropdown as the number of courses have increase

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { useLocation, useNavigate } from "react-router-dom";
 import SimpleQuestionAnswerView from "./SimpleQuestionAnswerView";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 type TabValue =
   | "math"
@@ -12,43 +18,46 @@ type TabValue =
   | "bigPicture";
 
 interface ISubjectConfig {
-  tabName: string;
   dataUrl: string;
   title: string;
 }
 
-const subjectConfig: ISubjectConfig[] = [
-  {
-    tabName: "math",
+const subjectConfig: { [key: string]: ISubjectConfig } = {
+  math: {
     dataUrl:
       "https://gist.githubusercontent.com/erdevanshusharma/104ae6bc9843f34512d0b1f559e7c582/raw/mathsQuestions.json",
     title: "Maths Challenge",
   },
-  {
-    tabName: "science",
+  science: {
     dataUrl:
       "https://gist.githubusercontent.com/erdevanshusharma/9e12748907fd91ce25a4d2fd23963e49/raw/scienceQuestions.json",
     title: "Science Challenge",
   },
-  {
-    tabName: "geography",
+
+  geography: {
     dataUrl:
       "https://gist.githubusercontent.com/erdevanshusharma/84c09f63952963e9e7bb2d24c91b2e63/raw/geographyQuestions.json",
     title: "Geography Challenge",
   },
-  {
-    tabName: "english",
+
+  english: {
     dataUrl:
       "https://gist.githubusercontent.com/erdevanshusharma/32f73472dc5793a88a0c69eb449b791d/raw/englishGrammarQuestions.json",
     title: "English Grammar Challenge",
   },
-  {
-    tabName: "Big Picture",
+
+  ethics: {
+    dataUrl:
+      "https://gist.githubusercontent.com/erdevanshusharma/3e04a3e7e1085ce5c6b8d7f997f93422/raw/ethics.json",
+    title: "Ethics Challenge",
+  },
+
+  "Big Picture": {
     dataUrl:
       "https://gist.githubusercontent.com/erdevanshusharma/56a206859b9138a9d71d2420cbebe738/raw/bigPicture.json",
     title: "Big Picture Challenge",
   },
-];
+};
 
 const MainPage = () => {
   const navigate = useNavigate();
@@ -74,8 +83,10 @@ const MainPage = () => {
     visible: { opacity: 1, y: 0 },
   };
 
+  const selectedConfig = subjectConfig[activeTab];
+
   return (
-    <div className="min-h-screen bg-gradient-to-br from-purple-400 via-pink-500 to-red-500 p-4 sm:p-8">
+    <div className="min-h-screen bg-gradient-to-br flex flex-col items-center from-purple-400 via-pink-500 to-red-500 p-4 sm:p-8">
       <motion.h1
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
@@ -84,41 +95,31 @@ const MainPage = () => {
       >
         Ayan's Learning Adventure
       </motion.h1>
-      <Tabs
-        value={activeTab}
-        onValueChange={(tab) => handleTabChange(tab as TabValue)}
-        className="w-full max-w-4xl mx-auto"
-      >
-        <TabsList
-          className={`grid w-full grid-cols-5 rounded-xl bg-white/20 p-1`}
-        >
-          {subjectConfig.map((config) => (
-            <TabsTrigger
-              value={config.tabName}
-              key={config.tabName}
-              className="rounded-lg text-white hover:bg-white/10 data-[state=active]:bg-white/30"
+      <Select onValueChange={(tab) => handleTabChange(tab as TabValue)}>
+        <SelectTrigger className="w-fit capitalize text-3xl px-12 rounded-full border-none bg-purple-400/50 focus:outline-none py-8 focus:ring-0 focus:border-none focus:ring-offset-0">
+          <SelectValue placeholder={selectedConfig.title} />
+        </SelectTrigger>
+        <SelectContent className=" flex flex-col p-2 rounded-lg justify-center max-h-screen">
+          {Object.entries(subjectConfig).map(([tabName, config]) => (
+            <SelectItem
+              value={tabName}
+              key={tabName}
+              className="items-center cursor-pointer"
             >
-              <p className="capitalize">{config.tabName}</p>
-            </TabsTrigger>
+              <p className="capitalize text-3xl px-8 py-4">{config.title}</p>
+            </SelectItem>
           ))}
-        </TabsList>
-        <motion.div
-          key={activeTab}
-          initial="hidden"
-          animate="visible"
-          variants={tabVariants}
-          transition={{ duration: 0.3 }}
-        >
-          {subjectConfig.map((config) => (
-            <TabsContent value={config.tabName} key={config.tabName}>
-              <SimpleQuestionAnswerView
-                dataUrl={config.dataUrl}
-                title={config.title}
-              />
-            </TabsContent>
-          ))}
-        </motion.div>
-      </Tabs>
+        </SelectContent>
+      </Select>
+      <motion.div
+        key={activeTab}
+        initial="hidden"
+        animate="visible"
+        variants={tabVariants}
+        transition={{ duration: 0.3 }}
+      >
+        <SimpleQuestionAnswerView dataUrl={selectedConfig.dataUrl} />
+      </motion.div>
     </div>
   );
 };
--- src/components/SimpleQuestionAnswerView.tsx
+++ src/components/SimpleQuestionAnswerView.tsx
@@ -30,13 +30,7 @@ interface SimpleQuestionAnswerData {
   explanation: string;
 }
 
-const SimpleQuestionAnswerView = ({
-  title,
-  dataUrl,
-}: {
-  title: string;
-  dataUrl: string;
-}) => {
+const SimpleQuestionAnswerView = ({ dataUrl }: { dataUrl: string }) => {
   const [simpleQuestions, setSimpleQuestions] = useState<
     SimpleQuestionAnswerData[]
   >([]);
@@ -113,7 +107,6 @@ const SimpleQuestionAnswerView = ({
   return (
     <div className="w-full max-w-4xl mx-auto p-4">
       <h1 className="text-3xl font-bold items-center mb-6 flex flex-col gap-2">
-        {title}
         {Score({ points: totalPoints, numQuestions: simpleQuestions.length })}
       </h1>
       {simpleQuestions.map((question, questionIndex) => (
